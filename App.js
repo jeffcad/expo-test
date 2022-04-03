5fcc0db8b94fd4db0604f75a19cd3e7339f23e88
Jeff Cadieux <jeffcadieux@hotmail.com>
2022-04-03 18:49:18 +0900

    Initial commit - alert button color test

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Alert, Button } from 'react-native'
 
 export default function App() {
+
+  const alert = () => {
+    Alert.alert('Android - Blue or Black?', 'OK is blue on Expo Go, black built with EAS')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Show Alert' onPress={alert} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
